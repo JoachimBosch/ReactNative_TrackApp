cb069faf6c64ac961e860805384dfbfe546cb939
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-11-10 11:24:17 +0100

    connected to MongoDb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+    "mongodb+srv://admin:passwordpassword@trackerapp.fdm42.mongodb.net/";
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to Mongo instance");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.error("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
     res.send("Hi There!");
