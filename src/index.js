8850bc790f0f770688b2891dda287d939faf4345
Joachim Bosch <164736830+JoachimBosch@users.noreply.github.com>
2024-11-13 17:17:52 +0100

    jwt + MongoDb models

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri =
     "mongodb+srv://admin:passwordpassword@trackerapp.fdm42.mongodb.net/";
@@ -16,8 +22,8 @@ mongoose.connection.on("error", (err) => {
     console.error("Error connecting to mongo", err);
 });
 
-app.get("/", (req, res) => {
-    res.send("Hi There!");
+app.get("/", requireAuth, (req, res) => {
+    res.send(`Your email is: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
